582ad02920ca4d4c9f09fdf9a3bc4a99ccf1b684
cloprothro@gmail.com <clostar.delta@gmail.com>
2022-10-16 17:50:44 -0700

    hopefully https on phone also

--- server.js
+++ server.js
@@ -5,3 +5,12 @@ const PORT = process.env.PORT || 3000;
 app.use(express.static('public'));
 
 app.listen(PORT, () => console.log('Bojack. Horseman. Obviously.'));
+
+if(process.env.NODE_ENV === 'production') {
+  app.use((req, res, next) => {
+    if (req.header('x-forwarded-proto') !== 'https')
+      res.redirect(`https://${req.header('host')}${req.url}`)
+    else
+      next()
+  })
+}
