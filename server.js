a13361daebecab0cd34fd447bbe344a1ac553ee4
cloprothro@gmail.com <clostar.delta@gmail.com>
2022-10-16 17:19:05 -0700

    I went to the business factory. I did a business

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
+const PORT = process.env.PORT || 3000;
 
 app.use(express.static('public'));
 
-app.listen(3000, () => console.log('Bojack. Horseman. Obviously.'));
+app.listen(PORT, () => console.log('Bojack. Horseman. Obviously.'));
